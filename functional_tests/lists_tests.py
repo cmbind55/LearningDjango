993e1cb0be546fee914636d93a8afadfbf6c5cf7
cmbind55 <cmbind55@live.com>
2015-08-31 11:11:05 -0400

    Make use of bootstrap stylesheets to modify index and lists.html
    Added divs to base.html to use jumbotron class.  Added a base.css
    file for overriding bootstrap.  Changed to StaticLiveServerTestCase
    in order for CSS to load in the functional tests.  Added table class
    tag in list.html.

--- functional_tests/lists_tests.py
+++ functional_tests/lists_tests.py
@@ -3,9 +3,10 @@ __author__ = 'cbinder'
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
 
-class LiveNewVisitorTest(LiveServerTestCase):
+class LiveNewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):  #2
         #chrome_option = webdriver.ChromeOptions()
         #chrome_option.add_argument('--proxy-server=us-auto.proxy.lexmark.com:80' )
@@ -96,14 +97,13 @@ class LiveNewVisitorTest(LiveServerTestCase):
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
-        time.sleep(10)
+        time.sleep(2)
 
         # Again, there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
-        time.sleep(10)
         self.assertIn('Buy milk', page_text)
-        time.sleep(10)
+        time.sleep(2)
 
         # Satisfied, they both go back to sleep
 
