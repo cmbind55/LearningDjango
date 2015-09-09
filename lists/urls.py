dafc5826bdc3e875bedc8f663b453cd88a19c9fd
cmbind55 <cmbind55@live.com>
2015-09-09 16:46:31 -0400

    Refactor list view to handle new item POSTs

--- lists/urls.py
+++ lists/urls.py
@@ -6,6 +6,5 @@ urlpatterns = patterns('',
     url(r'^$', views.home_page),
     url(r'^new$', views.new_list, name='new_list'),
     url(r'^(\d+)/$', views.view_list, name='view_list'),
-    url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
 
 )
--- lists/views.py
+++ lists/views.py
@@ -16,7 +16,9 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d/' % (list_.id,))
     return render(request, 'lists/list.html', {
         'page_title': 'To-Do',
         'list': list_,
@@ -33,10 +35,4 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'lists/index.html', {"error": error, 'page_title': 'To-Do'})
-    return redirect('/lists/%d/' % (list_.id,), {'page_title': 'To-Do'})
-
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/%d/' % (list_.id,), {'page_title': 'To-Do'})
