c1a80883e1fcdf09157474802040791a91f47660
cmbind55 <cmbind55@live.com>
2015-09-14 11:00:39 -0400

    refactor views to use form.save() instead of Item object create posts

--- lists/views.py
+++ lists/views.py
@@ -11,13 +11,14 @@ from django.core.exceptions import ValidationError
 def home_page(request):
     return render(request, 'lists/index.html', {'form': ItemForm()})
 
+
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     form = ItemForm()
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(request, 'lists/list.html', {'list': list_, "form": form})
 
@@ -26,7 +27,7 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'lists/index.html', {"form": form})
