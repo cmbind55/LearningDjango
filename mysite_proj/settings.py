f6d3c3ebd04219c2d44996ec315a33fbd3c3b1be
cmbind55 <cmbind55@live.com>
2016-01-29 09:47:42 -0500

    Add user model with email as only record

--- mysite_proj/settings.py
+++ mysite_proj/settings.py
@@ -67,6 +67,11 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = (
+    'accounts.authentication.PersonaAuthenticationBackend',
+)
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
