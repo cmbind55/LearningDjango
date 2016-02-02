66d0e2a03d13acd7383e66f81c7f090c0d73ce17
cmbind55 <cmbind55@live.com>
2016-02-02 14:24:56 -0500

    Fix hardcoded domain to resolve persona issue

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -34,10 +34,7 @@ def _get_latest_source(source_folder):
 def _update_settings(source_folder, site_name):
     settings_path = source_folder + '/mysite_proj/settings.py'
     sed(settings_path, "DEBUG = True", "DEBUG = False")
-    sed(settings_path,
-        'ALLOWED_HOSTS =.+$',
-        'ALLOWED_HOSTS = ["%s"]' % (site_name,)
-    )
+    sed(settings_path, 'DOMAIN = "localhost"', 'DOMAIN = "%s"' % (site_name,))
     secret_key_file = source_folder + '/mysite_proj/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
