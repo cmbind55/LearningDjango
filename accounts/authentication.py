5c16d761e37bf19894d0c47da14e9e692f25d1a9
cmbind55 <cmbind55@live.com>
2016-02-03 14:18:58 -0500

    baked in logging and cleaned up fetch prune in fabric script

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,14 +1,16 @@
-import requests
+import requests, logging
 from django.contrib.auth import get_user_model
 from django.conf import settings
-User = get_user_model()
 
+User = get_user_model()
+logger = logging.getLogger(__name__)
 PERSONA_VERIFY_URL = 'https://verifier.login.persona.org/verify'
 
 
 class PersonaAuthenticationBackend(object):
 
     def authenticate(self, assertion):
+        logger.warning('Entered class PersonaAuthenticationBackend, authenticate()')
         response = requests.post(
             PERSONA_VERIFY_URL,
             data={'assertion': assertion, 'audience': settings.DOMAIN}
@@ -19,6 +21,8 @@ class PersonaAuthenticationBackend(object):
                 return User.objects.get(email=email)
             except User.DoesNotExist:
                 return User.objects.create(email=email)
+        else:
+            logger.warning('Persona says no. Json was: {}'.format(response.json()))
 
     def get_user(self, email):
         try:
