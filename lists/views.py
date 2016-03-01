a71d645aa16d54b86d5f7e4d4a7ec0a9e757f5f5
cmbind55 <cmbind55@live.com>
2016-03-01 11:42:07 -0500

    start view controller and template for my lists feature

--- lists/views.py
+++ lists/views.py
@@ -33,3 +33,6 @@ def view_list(request, list_id):
             return redirect(list_)
     return render(request, 'lists/list.html', {'list': list_, "form": form})
 
+
+def my_lists(request, email):
+    return render(request, 'lists/my_lists.html')
