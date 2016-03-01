7e5ad2e75561f3c54a65844f2a89ebb85fd99f7e
cmbind55 <cmbind55@live.com>
2016-03-01 16:11:45 -0500

    new_list view tries to assign owner but cant

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,11 +1,15 @@
 from django.test import TestCase
 from lists.models import Item, List
 from django.utils.html import escape
+from django.contrib.auth import get_user_model
 from lists.forms import (
     DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
     ExistingListItemForm, ItemForm,
 )
+from django.http import HttpRequest
+from lists.views import new_list
 from unittest import skip
+User = get_user_model()
 
 # Create your tests here.
 
@@ -157,9 +161,23 @@ class NewListTest(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_list_owner_is_saved_if_user_is_authenticated(self):
+        request = HttpRequest()
+        request.user = User.objects.create(email='a@b.com')
+        request.POST['text'] = 'new list item'
+        new_list(request)
+        list_ = List.objects.first()
+        self.assertEqual(list_.owner, request.user)
 
 class MyListsTest(TestCase):
 
     def test_my_lists_url_renders_my_lists_template(self):
+        User.objects.create(email='a@b.com')
         response = self.client.get('/lists/users/a@b.com/')
         self.assertTemplateUsed(response, 'lists/my_lists.html')
+
+    def test_passes_correct_owner_to_template(self):
+        User.objects.create(email='wrong@owner.com')
+        correct_user = User.objects.create(email='a@b.com')
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertEqual(response.context['owner'], correct_user)
--- lists/views.py
+++ lists/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from lists.models import Item, List
 from lists.forms import ExistingListItemForm, ItemForm
 from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 #
@@ -16,7 +19,9 @@ def home_page(request):
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
-        list_ = List.objects.create()
+        list_ = List()
+        list_.owner = request.user
+        list_.save()
         form.save(for_list=list_)
         return redirect(list_)
     else:
@@ -35,4 +40,5 @@ def view_list(request, list_id):
 
 
 def my_lists(request, email):
-    return render(request, 'lists/my_lists.html')
+    owner = User.objects.get(email=email)
+    return render(request, 'lists/my_lists.html', {'owner': owner})
