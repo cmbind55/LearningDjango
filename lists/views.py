1fb6e46bd5a18430e71b0fd196820c2f4d4d4d21
cmbind55 <cmbind55@live.com>
2016-03-02 13:15:15 -0500

    saving list owners by leveraging forms

--- lists/views.py
+++ lists/views.py
@@ -4,6 +4,7 @@ from lists.models import Item, List
 from lists.forms import ExistingListItemForm, ItemForm
 from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
+from lists.forms import ExistingListItemForm, ItemForm, NewListForm
 
 User = get_user_model()
 
@@ -20,12 +21,21 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List()
-        list_.owner = request.user
+        if request.user.is_authenticated():
+            list_.owner = request.user
         list_.save()
         form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'lists/index.html', {"form": form})
+
+
+def new_list2(request):
+    form = NewListForm(data=request.POST)
+    if form.is_valid():
+        list_ = form.save(owner=request.user)
+        return redirect(list_)
+    return render(request, 'lists/index.html', {'form': form})
 
 
 def view_list(request, list_id):
